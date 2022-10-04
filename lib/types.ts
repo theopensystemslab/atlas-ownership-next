ef6c3baa62b0f282b8d1ced2046966a5ef9996ef
Jessica McInchak <jessica.mcinchak@gmail.com>
2022-10-04 21:31:21 +0200

    expand entry type

--- lib/types.ts
+++ lib/types.ts
@@ -8,9 +8,35 @@ export type Location = {
   region: string
 }
 
+export type IntensityRange = 0|1|2|3|4|5
+
+export type Term = {
+  description: string
+  rightsIntensity?: IntensityRange
+  obligationIntensity?: IntensityRange
+}
+
+export type Reference = {
+  name: string
+  link: string
+  source: string
+}
+
 export type Entry = {
+  name: string
+  description: string
   location: Location
+  dates: {
+    start: string
+    end?: string
+  }
+  mainImage: {
+    source: string
+    credit: string
+  }
+  references: Reference[]
   slug: {
     current: string
   }
+  terms: Term[]
 }
