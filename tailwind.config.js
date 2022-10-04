02dc835788e91732b3dbd634f7537a2e65a6e5c6
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2022-10-04 17:12:43 +0100

    feat: Add AoO colours to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./**/*.tsx"],
   theme: {
+    colors: {
+      rent: "#BC87FF",
+      transfer: "#88C6FF",
+      administration: "#E92FAC",
+      eligibility: "#EC3E55",
+      security: "#FFA500",
+      develop: "#49C6A6",
+      stewardship: "#9FF876",
+      use: "#FEFC41",
+    },
     extend: {},
   },
   plugins: [],
