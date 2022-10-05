d5df0554504afd1c585c943bf30e977e2a7dc078
Jessica McInchak <jessica.mcinchak@gmail.com>
2022-10-05 14:39:59 +0200

    chore: expand Entry type (#3)

--- lib/types.ts
+++ lib/types.ts
@@ -8,9 +8,51 @@ export type Location = {
   region: string
 }
 
+export type IntensityRange = 0|1|2|3|4|5
+
+export type Term = {
+  description: string
+  pattern: Pattern
+  rightsIntensity?: IntensityRange
+  obligationIntensity?: IntensityRange
+}
+
+export type Pattern = {
+  name: string
+  description: string
+  class: PatternClass
+}
+
+export type PatternClass = {
+  name: string
+  description: string
+  order: number
+  color: {
+    hex: string
+  }
+}
+
+export type Reference = {
+  name: string
+  link: string
+  source: string
+}
+
 export type Entry = {
+  name: string
+  description: string
   location: Location
+  dates: {
+    start: string
+    end?: string
+  }
+  mainImage: {
+    source: string
+    credit: string
+  }
+  references: Reference[]
   slug: {
     current: string
   }
+  terms: Term[]
 }
