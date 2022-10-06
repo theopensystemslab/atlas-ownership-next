fe746432aa63a785e8968e8ac25752a4b0f2b250
Thomas O'Neill <tom@opensystemslab.io>
2022-10-06 17:54:28 +0100

    ..

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,5 @@
-import type { GetStaticProps, InferGetStaticPropsType } from "next"
-import { useEffect } from "react"
-import { entriesQuery } from "../lib/queries"
-import { sanityClient } from "../lib/sanity.server"
-import store from "../lib/store"
-import { Entry } from "../lib/types"
-
-const Home = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
-  const { entries } = props
-
-  useEffect(() => {
-    store.entries = entries
-  }, [entries])
-
+const Home = () => {
   return null
-}
-
-export const getStaticProps: GetStaticProps<{ entries: Entry[] }> = async ({
-  preview = false,
-}) => {
-  const entries = await sanityClient.fetch<Entry[]>(entriesQuery)
-  return {
-    props: { entries },
-    // If webhooks isn't setup then attempt to re-generate in 1 minute intervals
-    revalidate: process.env.SANITY_REVALIDATE_SECRET ? undefined : 60,
-  }
 }
 
 export default Home
