d91b5ce91a462bd2f45d8409b4455881e0038680
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-01-13 14:35:58 +0000

    chore: Setup colour channels for opacity to work

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        rent: "#B196FE",
-        transfer: "#87D4FF",
-        administration: "#e2e2e2",
-        eligibility: "#FF6176",
-        security: "#FF776F",
-        develop: "#D2F964",
-        stewardship: "#91EC66",
-        use: "#FAFF00",
-        access: "#FFA337",
+        rent: 'rgb(var(--color-rent) / <alpha-value>)',
+        transfer: 'rgb(var(--color-transfer) / <alpha-value>)',
+        administration: 'rgb(var(--color-administration) / <alpha-value>)',
+        eligibility: 'rgb(var(--color-eligibility) / <alpha-value>)',
+        security: 'rgb(var(--color-security) / <alpha-value>)',
+        develop: 'rgb(var(--color-develop) / <alpha-value>)',
+        stewardship: 'rgb(var(--color-stewardship) / <alpha-value>)',
+        use: 'rgb(var(--color-use) / <alpha-value>)',
       },
     },
   },
