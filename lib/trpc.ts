bc8fc773a6086b3e79d37de775916c391e00c670
Thomas O'Neill <tom@opensystemslab.io>
2023-01-15 18:58:15 -0800

    test

--- lib/trpc.ts
+++ lib/trpc.ts
@@ -33,6 +33,20 @@ export const trpc = createTRPCNext<AppRouter>({
            * @link https://trpc.io/docs/ssr
            **/
           url: `${getBaseUrl()}/api/trpc`,
+          headers() {
+            if (ctx?.req) {
+              // To use SSR properly, you need to forward the client's headers to the server
+              // This is so you can pass through things like cookies when we're server-side rendering
+              // If you're using Node 18, omit the "connection" header
+              const { connection: _connection, ...headers } = ctx.req.headers
+              return {
+                ...headers,
+                // Optional: inform server that it's an SSR request
+                "x-ssr": "1",
+              }
+            }
+            return {}
+          },
         }),
       ],
       /**
@@ -44,6 +58,6 @@ export const trpc = createTRPCNext<AppRouter>({
   /**
    * @link https://trpc.io/docs/ssr
    **/
-  ssr: false,
+  ssr: true,
 })
 // => { useQuery: ..., useMutation: ...}
