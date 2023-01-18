c56eea635f12cd0d205dd35e80b5464d729ae502
Thomas O'Neill <tom@opensystemslab.io>
2023-01-18 11:30:14 -0800

    wip #3

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import css from "@/styles/Sidebar.module.css"
+import css from "./Sidebar.module.css"
 import { pipe } from "fp-ts/lib/function"
 import { motion } from "framer-motion"
 import { useState } from "react"
@@ -6,20 +6,21 @@ import theme from "tailwindcss/defaultTheme"
 import { A } from "../../lib/fp"
 import { trpc } from "../../lib/trpc"
 import PatternClassAccordion from "./PatternClassAccordion"
+import { ChevronLeft, ChevronRight } from "@carbon/icons-react"
 
 const Chevvy = (props: any) => (
   <div className={css.chevvy} {...props}>
     <motion.div
       variants={{
         closed: {
-          x: `${theme.spacing[6]}`,
+          x: `${theme.spacing[8]}`,
         },
         open: {
           x: 0,
         },
       }}
     >
-      {props?.open ? `<` : `>`}
+      {props?.open ? <ChevronLeft size={24} /> : <ChevronRight size={24} />}
     </motion.div>
   </div>
 )
