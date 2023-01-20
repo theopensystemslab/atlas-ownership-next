6574f77e7471a154ff733301c26596926948d09c
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-01-20 14:19:38 +0000

    fix: Standard header on patterns page, fix font colour

---
+++ components/PageNavbar.tsx
@@ -0,0 +1,15 @@
+import { Close } from "@carbon/icons-react"
+import Link from "next/link"
+
+export const PageNavbar = (props: { variant?: "light" }) => {
+  const { variant } = props
+
+  return (
+    <nav className={`flex justify-between ml-8 mr-8 mt-3 h-12 ${variant == "light" ? "text-black" : ""}`}>
+      <Link href="/" className="cursor-pointer">Atlas of Ownership</Link>
+      <Link href="/" className="text-lg">
+        <Close size={24} className="cursor-pointer" />
+      </Link>
+    </nav>
+  )
+}
