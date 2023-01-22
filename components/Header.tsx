89722003018a42813dcd40fe6011c8ecb39f30d7
Thomas O'Neill <tom@opensystemslab.io>
2023-01-21 21:13:38 -0800

    search with results first pass

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,13 +2,14 @@ import Search from "./Search"
 
 const Header = () => (
   <header className="absolute z-10 w-full flex justify-between">
-    <div className="p-10 w-1/4 text-white">
+    {/* <div className="p-10 w-1/4 text-white">
       <h1 className="text-4xl fill-black">The Atlas of Ownership</h1>
       <h2 className="text-lg pt-3">
         A map of property rights and obligations across time and space
       </h2>
       <a className="text-lg text-gray-500">Why?</a>
-    </div>
+    </div> */}
+    <div />
     <Search />
   </header>
 )
