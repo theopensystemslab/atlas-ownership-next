3233fc7cc0239bb2188a32a6a4489771b04e8db9
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-01-27 09:13:12 +0000

    fix: Use SVG OSL logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -42,15 +42,13 @@ const Contributors = () => {
 }
 
 const OSLLogo = () => (
-  <div>
-    <Image
-      src="/logos/osl.png"
-      alt="Open System Lab logo"
-      width="24px"
-      height="32px"
-      layout="fixed"
-      className="hover:brightness-50"
-    />
+  <div className="h-5 w-5 mt-0.5">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 140 196">
+      <path
+        fill="currentColor"
+        d="M84.07 28.36h27.86V.5H28.36v27.86h55.71zm27.86 0v139.28h27.86V28.36h-27.86zm-83.57 83.57V28.36H.5v139.28h27.86v-55.71zm27.85 55.71H28.36v27.86h83.57v-27.86H56.21z"
+      />
+    </svg>
   </div>
 )
 
@@ -71,7 +69,7 @@ const SocialIcons = () => {
   ]
   
   return (
-    <div className="flex space-x-3">
+    <div className="grid grid-cols-4 gap-3">
       { socialLinks.map((link, i) => (
         <a 
           key={`social-link-${i + 1}`} 
