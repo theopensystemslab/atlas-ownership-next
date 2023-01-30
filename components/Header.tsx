794841226651968a1abbfd565f989c190baa0963
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-01-30 20:46:53 +0000

    feat: Add header links

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import css from "./Header.module.css"
 import Search from "./Search"
 
 const Header = () => (
-  <header className="absolute z-10 w-full flex justify-between pointer-events-none">
+  <header className="absolute z-10 w-full flex justify-between pointer-events-none text-sm">
     <div className="pl-16 pt-20 sm:pt-10 pr-5 w-3/4 sm:w-1/2 md:w-1/4 text-white">
       <h1 className={css.title}>The Atlas of Ownership</h1>
       <h2 className="text-sm sm:text-lg pt-3">
@@ -13,7 +13,16 @@ const Header = () => (
         <a className="pointer-events-auto mt-2 text-sm sm:text-lg opacity-75">Why?</a>
       </Link>
     </div>
-    <Search />
+    <div className="h-fit mt-4 flex gap-10 pointer-events-auto text-white">
+      <Link href="/patterns">
+        <a className={css.link}>Patterns</a>
+        </Link>
+      <Link href="/about">
+        <a className={css.link}>About</a>
+      </Link>
+      <Search />
+
+    </div>
   </header>
 )
 
