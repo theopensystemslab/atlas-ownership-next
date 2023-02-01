d3aea0e92a737efd54a703e31bc5519aa12a9c1c
Thomas O'Neill <tom@opensystemslab.io>
2023-02-01 14:58:17 -0800

    next seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
 import * as Fathom from "fathom-client"
 import { NextPage } from "next"
+import { DefaultSeo } from "next-seo"
 import type { AppProps } from "next/app"
-import Head from "next/head"
 import { useRouter } from "next/router"
 import { ReactElement, ReactNode, useEffect } from "react"
 import GlobeLayout from "../components/layout/GlobeLayout"
 import { trpc } from "../lib/trpc"
+import SEO from "../next-seo.config"
 import "../styles/globals.css"
-
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode
 }
@@ -18,7 +18,7 @@ type AppPropsWithLayout = AppProps & {
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? GlobeLayout
-  const router = useRouter();
+  const router = useRouter()
 
   useEffect(() => {
     // Initialize Fathom when the app loads
@@ -27,28 +27,27 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
     //  - This must be an exact match of your domain.
     //  - If you're using www. for your domain, make sure you include that here.
     Fathom.load(process.env.NEXT_PUBLIC_FATHOM_TRACKING_CODE!, {
-      includedDomains: ['atlasofownership.org'],
-    });
+      includedDomains: ["atlasofownership.org"],
+    })
 
     function onRouteChangeComplete() {
-      Fathom.trackPageview();
+      Fathom.trackPageview()
     }
     // Record a pageview when route changes
-    router.events.on('routeChangeComplete', onRouteChangeComplete);
+    router.events.on("routeChangeComplete", onRouteChangeComplete)
 
     // Unassign event listener
     return () => {
-      router.events.off('routeChangeComplete', onRouteChangeComplete);
-    };
-  }, []);
+      router.events.off("routeChangeComplete", onRouteChangeComplete)
+    }
+  }, [router.events])
 
   return getLayout(
     <>
-      <Head>
-        <title>The Atlas of Ownership</title>
-      </Head>
+      <DefaultSeo {...SEO} />
       <Component {...pageProps} />
-    </>)
+    </>
+  )
 }
 
 export default trpc.withTRPC(MyApp as any)
