9b7d3c26fdbfc525fcc8a990d7553797b988a930
Thomas O'Neill <tom@opensystemslab.io>
2023-02-01 13:50:41 -0800

    wip another ssg query

--- pages/patterns.tsx
+++ pages/patterns.tsx
@@ -4,32 +4,43 @@ import { GetStaticProps, InferGetStaticPropsType } from "next"
 import Head from "next/head"
 import SuperJSON from "superjson"
 import NoopLayout from "../components/layout/NoopLayout"
+import { Page, PatternClass } from "../lib/types"
 import { appRouter } from "../server/routers/_app"
 
-export const getStaticProps: GetStaticProps = async (context) => {
+export const getStaticProps: GetStaticProps<{
+  patternClasses: PatternClass[]
+  patternsPageData: Page
+}> = async (context) => {
   const ssg = await createProxySSGHelpers({
     router: appRouter,
     ctx: {},
     transformer: SuperJSON, // optional - adds superjson serialization
   })
+
   const patternClasses = await ssg.patternClasses.fetch()
+  const patternsPageData = await ssg.page.fetch({ pageSlug: "patterns" })
 
   return {
     props: {
       patternClasses,
+      patternsPageData,
     },
   }
 }
 
-const PatternsPage = ({
-  patternClasses,
-}: InferGetStaticPropsType<typeof getStaticProps>) => {
+const PatternsPage = (
+  props: InferGetStaticPropsType<typeof getStaticProps>
+) => {
+  const { patternClasses, patternsPageData } = props
   return (
     <>
       <Head>
         <title>Explore the patterns - The Atlas of Ownership</title>
       </Head>
-      <PatternsLayout patternClasses={patternClasses} />
+      <PatternsLayout
+        patternClasses={patternClasses}
+        patternsPageData={patternsPageData}
+      />
     </>
   )
 }
